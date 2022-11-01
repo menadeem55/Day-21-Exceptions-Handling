566b8f9280e44e56c6b1584031d8e6ba3f958436
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-11-01 15:42:55 +0530

    [Added: To Analyse message and return Happy]

--- src/com/moodAnalyzer/java/MoodAnalyzerTest.java
+++ src/com/moodAnalyzer/java/MoodAnalyzerTest.java
@@ -3,22 +3,21 @@ package com.moodAnalyzer.java;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-/**
- * To Check the Mood of the Person by String Entered. Emphasis on - Junit Test
- * Cases - Exception Handling - Custom Exceptions Author
- *
- * Testing Class of MoodAnalyzerTest Created Object of Mood Analyzer Created a
- * Test Function and Imported Assertions and checking SAD or HAPPY
- */
-
 public class MoodAnalyzerTest {
 	MoodAnalyzer moodAnalyze = new MoodAnalyzer();
 
 	@Test
 	public void if_MessageWasSad_ShouldReturnSad() {
-
 		String message = "I am in Sad Mood";
-		String result = moodAnalyze.moodCheck(message);
-		Assertions.assertEquals("SAD", result);
+		String result1 = moodAnalyze.moodCheck(message);
+		Assertions.assertEquals("SAD", result1);
 	}
-} 
+
+	@Test
+	public void if_MessageWasHAppy_ShouldReturnHappy() {
+		String message = "I am in Happy Mood";
+		String result2 = moodAnalyze.moodCheck(message);
+		Assertions.assertEquals("HAPPY", result2);
+
+	}
+}
