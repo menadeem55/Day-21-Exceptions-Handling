af20cdcd11fd8309e5848854ec762b11fdb8330c
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-11-01 10:49:26 +0530

    [Added: To Analyse and Respond Happy or Sab Mood]

--- src/com/moodAnalyzer/java/MoodAnalyzer.java
+++ src/com/moodAnalyzer/java/MoodAnalyzer.java
@@ -3,8 +3,6 @@ package com.moodAnalyzer.java;
 /**
  * To Check the Mood of the Person by String Entered. Emphasis on - Junit Test
  * Cases - Exception Handling - Custom Exceptions
- *
- * 
  */
 //Mood Analyzer Class
 
@@ -12,7 +10,7 @@ public class MoodAnalyzer {
 
 	 //Function to Check Mood of a String Entered
    public String moodCheck(String message){
-       if(message.contains("Sad")) {
+       if(message.contains("SAD")) {
            System.out.println("In SAD Mood");
            return "SAD";
        }
