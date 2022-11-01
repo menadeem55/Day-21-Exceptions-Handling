74f5c6f72ba7323ec4ad66e08765e6a95e8be9d0
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-11-01 16:03:11 +0530

    [Added: To refactored code return Happy and Sad]

--- src/com/moodAnalyzer/java/MoodAnalyzer.java
+++ src/com/moodAnalyzer/java/MoodAnalyzer.java
@@ -4,19 +4,25 @@ package com.moodAnalyzer.java;
  * To Check the Mood of the Person by String Entered. Emphasis on - Junit Test
  * Cases - Exception Handling - Custom Exceptions
  */
+
 //Mood Analyzer Class
 
 public class MoodAnalyzer {
+String message;
+	
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
 
 	 //Function to Check Mood of a String Entered
-   public String moodCheck(String message){
-       if(message.contains("SAD")) {
-           System.out.println("In SAD Mood");
-           return "SAD";
-       }
-       else{
-           System.out.println("In HAPPY Mood");
-           return "HAPPY";
-       }
+    public String moodCheck(){
+        if(message.contains("Sad")) {
+            System.out.println("In SAD Mood");
+            return "SAD";
+        }
+        else{
+            System.out.println("In HAPPY Mood");
+            return "HAPPY";
+        }
 	}
 }
--- src/com/moodAnalyzer/java/MoodAnalyzerTest.java
+++ src/com/moodAnalyzer/java/MoodAnalyzerTest.java
@@ -3,21 +3,28 @@ package com.moodAnalyzer.java;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+/**
+ * To Check the Mood of the Person by String Entered. Emphasis on - Junit Test
+ * Cases - Exception Handling - Custom Exceptions Author
+ *
+ * Testing Class of MoodAnalyzerTest Created Object of Mood Analyzer Created a
+ * Test Function and Imported Assertions and checking SAD or HAPPY
+ */
 public class MoodAnalyzerTest {
-	MoodAnalyzer moodAnalyze = new MoodAnalyzer();
+	
 
-	@Test
-	public void if_MessageWasSad_ShouldReturnSad() {
-		String message = "I am in Sad Mood";
-		String result1 = moodAnalyze.moodCheck(message);
-		Assertions.assertEquals("SAD", result1);
+	 @Test
+	    public void if_MessageWasSad_ShouldReturnSad(){
+		 MoodAnalyzer moodAnalyze = new MoodAnalyzer("I am in Sad Mood");
+	        
+	        String result1 = moodAnalyze.moodCheck();
+	        Assertions.assertEquals("SAD",result1);
 	}
+	 @Test
+	    public void if_MessageWasHappy_ShouldReturnHappy() {
+		 MoodAnalyzer moodAnalyze = new MoodAnalyzer("I am in Happy Mood");
 
-	@Test
-	public void if_MessageWasHAppy_ShouldReturnHappy() {
-		String message = "I am in Happy Mood";
-		String result2 = moodAnalyze.moodCheck(message);
-		Assertions.assertEquals("HAPPY", result2);
-
-	}
+		 String result2 = moodAnalyze.moodCheck();
+		 Assertions.assertEquals("HAPPY", result2);
+	 }
 }
